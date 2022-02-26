c06ed8e7b55a715e195d985582cc0cc892471192
lenilase <leni.lase@student.uh.ac.id>
2022-02-26 13:31:46 +0700

    var

---
+++ variabel.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var islogin bool = false
+	fmt.Println(islogin)
+
+	var prime int = 7
+	fmt.Println(prime)
+
+	var decimal float64 = 45.6
+	fmt.Println(decimal)
+
+	var hello string = "hello World"
+	fmt.Println(hello)
+
+	const pi = 3.14
+	fmt.Println(pi)
+}
